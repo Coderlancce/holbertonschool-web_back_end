8305a1b3c789f5ab3edb76b26ecda22f24eee742
Coderlancce <coderlancce@gmail.com>
2023-05-22 10:58:36 -0500

    OK convert float to int

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function floor
+which takes a float n as argument and
+returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    return a int
+    """
+    return int(n)
