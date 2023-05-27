a61485712944e6b43098e5f3def4b5b509a2529e
Coderlancce <coderlancce@gmail.com>
2023-05-27 14:48:05 -0500

    OK mru

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,41 +1,47 @@
 #!/usr/bin/python3
-"""CCreate a class MRUCache that inherits from
-BaseCaching and is a caching system
-"""
-from base_caching import BaseCaching
-from functools import lru_cache
+''' MRU Caching: Create a class MRUCache that inherits from BaseCaching
+                 and is a caching system
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """least recently used caching system"""
-
-    USED = {}
+    ''' An MRU Cache.
+        Inherits all behaviors from BaseCaching except, upon any attempt to
+        add an entry to the cache when it is at max capacity (as specified by
+        BaseCaching.MAX_ITEMS), it discards the most recently used entry to
+        accommodate for the new one.
+        Attributes:
+          __init__ - method that initializes class instance
+          put - method that adds a key/value pair to cache
+          get - method that retrieves a key/value pair from cache '''
 
     def __init__(self):
+        ''' Initialize class instance. '''
         super().__init__()
+        self.keys = []
 
     def put(self, key, item):
-        """Add chaching to caching system"""
-        if key is None or item is None:
-            pass
-        else:
-            self.cache_data.update({key: item})
-            self.USED.update({key: 0})
-            if self.cache_data.__len__() > super().MAX_ITEMS:
-                pop_item = max(self.USED)
-                if self.cache_data.get(pop_item) is not None:
-                    self.USED.pop(pop_item)
-                    self.cache_data.pop(pop_item)
-                print("DISCARD: {}".format(pop_item))
+        ''' Add key/value pair to cache data.
+            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
+            discard most recently used entry in cache to accommodate new
+            entry. '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(-2)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        """Get cache from caching system"""
-        if key is None or self.cache_data.get(key) is None:
-            return None
-
-        if self.USED.get(key) is not None:
-            self.USED[key] += 1
-        else:
-            self.USED[key] = 1
-
-        return self.cache_data.get(key)
+        ''' Return value stored in `key` key of cache.
+            If key is None or does not exist in cache, return None. '''
+        if key is not None and key in self.cache_data:
+            self.keys.append(self.keys.pop(self.keys.index(key)))
+            return self.cache_data[key]
+        return None
